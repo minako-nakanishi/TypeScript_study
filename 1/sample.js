3f90dde641cf22386b72afd9d23f89385a317e4a
Minako Nakanishi <n135216e@gmail.com>
2019-04-01 16:44:46 +0900

    型宣言をしなくても良い

--- 1/sample.js
+++ 1/sample.js
@@ -1,3 +1,6 @@
 var data1 = 'hoge'; //String型の宣言
 data1 = 'foo';
-console.log(data1);
+console.log(data1); //foo
+var data2 = 100; //型宣言をしなくても良い
+data2 = 150;
+console.log(data2);
--- 1/sample.ts
+++ 1/sample.ts
@@ -1,3 +1,7 @@
 let data1:string = 'hoge'; //String型の宣言
 data1 = 'foo';
-console.log(data1);
+console.log(data1); //foo
+
+let data2 = 100; //型宣言をしなくても良い(型推論：初期値から型を推測する)
+data2 = 150;
+console.log(data2); //150
