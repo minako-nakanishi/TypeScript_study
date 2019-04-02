a06c605b851cb181fe45c30e81f767e18d2fa2dd
Minako Nakanishi <n135216e@gmail.com>
2019-04-02 10:21:02 +0900

    配列の宣言

---
+++ let_var/hairetsu.js
@@ -0,0 +1,4 @@
+/** 配列の宣言 */
+var data = ['Java', 'Python', 'PHP', 'Ruby'];
+console.log(data); //[ 'Java', 'Python', 'PHP', 'Ruby' ]
+console.log(data[0]);
---
+++ let_var/hairetsu.ts
@@ -0,0 +1,4 @@
+/** 配列の宣言 */
+let data:string[] = ['Java', 'Python', 'PHP', 'Ruby']; //データ型の後ろに[]を付加する
+console.log(data); //[ 'Java', 'Python', 'PHP', 'Ruby' ]
+console.log(data[0]); //Java
