d682e6c98c2c0d40f3d491d4ea4ea4776f2e93a1
Minako Nakanishi <n135216e@gmail.com>
2019-04-02 10:42:00 +0900

    多次元配列と空の配列の宣言方法

--- let_var/hairetsu.js
+++ let_var/hairetsu.js
@@ -1,4 +1,7 @@
 /** 配列の宣言 */
-var data = ['Java', 'Python', 'PHP', 'Ruby'];
+var data = ['Java', 'Python', 'PHP', 'Ruby']; //データ型の後ろに[]を付加する
 console.log(data); //[ 'Java', 'Python', 'PHP', 'Ruby' ]
-console.log(data[0]);
+console.log(data[0]); //Java
+/** 多次元配列の宣言 */
+var fruits = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(fruits[1][1]);
--- let_var/hairetsu.ts
+++ let_var/hairetsu.ts
@@ -2,3 +2,11 @@
 let data:string[] = ['Java', 'Python', 'PHP', 'Ruby']; //データ型の後ろに[]を付加する
 console.log(data); //[ 'Java', 'Python', 'PHP', 'Ruby' ]
 console.log(data[0]); //Java
+
+/** 多次元配列の宣言 */
+let fruits:number[][] = [[1,2,3],[4,5,6],[7,8,9]];
+console.log(fruits[1][1]); //5
+
+/** 空の配列の宣言方法(最も良いVer)シンプルだかららしいです */
+let animal:string[] = [];
+
