8510e13b0ed7c84922acd9c2145f774d84924aa2
Minako Nakanishi <n135216e@gmail.com>
2019-04-02 09:53:18 +0900

    let, varの禁止事項

--- let_var/hensu.js
+++ let_var/hensu.js
@@ -1,2 +1,10 @@
+/**
+ * letで同じ変数名で宣言を試みるとエラーとなる
+ */
 var app = 1;
-var app = 2;
+// let app:number = 2; //Cannot redeclare block-scoped variable 'app'.
+/**
+ * varで同じ変数名だた違う型で宣言した場合(エラーとなる)
+ */
+var sam = 100;
+// var sam: string = 'hoge'; //Subsequent variable declarations must have the same type.  Variable 'sam' must be of type 'number', but here has type 'string'.
--- let_var/hensu.ts
+++ let_var/hensu.ts
@@ -3,3 +3,10 @@
  */
 let app: number = 1;
 // let app:number = 2; //Cannot redeclare block-scoped variable 'app'.
+
+
+/**
+ * varで同じ変数名だた違う型で宣言した場合(エラーとなる)
+ */
+var sam: number = 100;
+// var sam: string = 'hoge'; //Subsequent variable declarations must have the same type.  Variable 'sam' must be of type 'number', but here has type 'string'.
